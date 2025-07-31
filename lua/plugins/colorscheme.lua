c5aa1062616467ea76adc72c0b737407f634b54f
austinburnett <aburn1011@gmail.com>
2025-07-31 16:54:47 -0400

    Make the Constant highlight group be the same color as variables

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,15 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
         -- load the colorscheme here
+
+        require('kanagawa').setup({
+            overrides = function(colors)
+                return {
+                    -- Assign a static color to strings
+                    Constant = { fg = colors.palette.fujiWhite }
+                }
+            end
+        })
         vim.cmd([[colorscheme kanagawa]])
     end,
 }
