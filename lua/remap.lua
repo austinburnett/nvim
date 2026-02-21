1e15d5d9810917d250fcd6634e44c3ad5c7a1afb
austinburnett <aburn1011@gmail.com>
2026-02-21 14:05:40 -0500

    Move things around and change windows resize key

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,6 +1,9 @@
 -- Leader key is spacebar
 vim.g.mapleader = " "
 
+-- Go from terminal mode to Normal mode
+vim.keymap.set("t", "<C-[>", "<C-\\><C-N>")
+
 -- Copy to clipboard
 vim.keymap.set("v", "<leader>y", "\"*y")
 
@@ -16,45 +19,33 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
 
--- Navigating window splits (Very nicee)
-vim.keymap.set("n", "<C-h>", ":wincmd h<cr>", {
-    silent = true
-})
-vim.keymap.set("n", "<C-j>", ":wincmd j<cr>", {
-    silent = true
-})
-vim.keymap.set("n", "<C-k>", ":wincmd k<cr>", {
-    silent = true
-})
-vim.keymap.set("n", "<C-l>", ":wincmd l<cr>", {
-    silent = true
-})
+-- Navigating window splits
+vim.keymap.set("n", "<C-h>", ":wincmd h<cr>", { silent = true })
+vim.keymap.set("n", "<C-j>", ":wincmd j<cr>", { silent = true })
+vim.keymap.set("n", "<C-k>", ":wincmd k<cr>", { silent = true })
+vim.keymap.set("n", "<C-l>", ":wincmd l<cr>", { silent = true })
 
--- Go from terminal mode to Normal mode
-vim.keymap.set("t", "<C-[>", "<C-\\><C-N>")
-
--- Resizing window splits (Very nicee)
+-- Resizing window splits
 -- MacOS doesn't play well with the alt remaps: <M-symbol>
 -- Check to see what symbol MacOS sends by putting nvim in
 -- insert mode and typing in Ctrl-V <Alt-,>
 
---vim.keymap.set("n", "<M-,>", "<C-W>5<")
+-- Windows
+vim.keymap.set("n", "<M-,>", "<C-W>5<")
+vim.keymap.set("n", "<M-.>", "<C-W>5>")
+vim.keymap.set("n", "<M-s>", "<C-W>-")
+vim.keymap.set("n", "<M-b>", "<C-W>+")
+
+-- MacOS
 vim.keymap.set("n", "≤", "<C-W>5<")
---vim.keymap.set("n", "<M-.>", "<C-W>5>")
 vim.keymap.set("n", "≥", "<C-W>5>")
-
---vim.keymap.set("n", "<M-s>", "<C-W>-")
 vim.keymap.set("n", "ß", "<C-W>-")
---vim.keymap.set("n", "<M-t>", "<C-W>+")
 vim.keymap.set("n", "∫", "<C-W>+")
-
--- Show file hierarchy
-vim.keymap.set("n", "`", ":NeoTreeFocusToggle <cr>")
 
 -- Lsp Remaps
 vim.keymap.set("n", "gd", vim.lsp.buf.declaration)
 
--- Debuging via dap
+-- Debuging
 vim.fn.sign_define('DapBreakpoint', {text='🔴', texthl='', linehl='', numhl=''})
 vim.fn.sign_define('DapStopped',{ text ='', texthl ='', linehl='Search', numhl =''})
 vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
@@ -62,22 +53,3 @@ vim.keymap.set('n', '<F9>', function() require('dap').toggle_breakpoint() end)
 vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
 vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
 vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
-vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
-vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
-vim.keymap.set('n', '<Leader>lp', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
-vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
-vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
-    require('dap.ui.widgets').hover()
-end)
-vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
-    require('dap.ui.widgets').preview()
-end)
-vim.keymap.set('n', '<Leader>df', function()
-    local widgets = require('dap.ui.widgets')
-    widgets.centered_float(widgets.frames)
-end)
-vim.keymap.set('n', '<Leader>ds', function()
-    local widgets = require('dap.ui.widgets')
-    widgets.centered_float(widgets.scopes)
-end)
