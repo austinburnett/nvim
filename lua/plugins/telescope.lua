d29e2882bf4139c729716f45a9a84b1d904d3662
austinburnett <aburn1011@gmail.com>
2026-06-17 20:33:45 -0400

    Stylize telescope file

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,16 @@
-return {
+return
+{
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.8',
-        dependencies = {'nvim-lua/plenary.nvim'},
+        -- Using tag='*' pins the installation to the latest telescope release.
+        'nvim-telescope/telescope.nvim', tag = '*',
+        dependencies = { 'nvim-lua/plenary.nvim' },
         config = function ()
-            local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+            local telescopeBuiltIn = require('telescope.builtin')
 
+            vim.keymap.set('n', '<leader>ff', telescopeBuiltIn.find_files, {})
+            vim.keymap.set('n', '<leader>fg', telescopeBuiltIn.live_grep, {})
+            vim.keymap.set('n', '<leader>fb', telescopeBuiltIn.buffers, {})
+            vim.keymap.set('n', '<leader>fh', telescopeBuiltIn.help_tags, {})
         end
     }
 }
