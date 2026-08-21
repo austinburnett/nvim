33b1aecbbab3f61d1dec6d3a5ecd932a7d4025e4
austinburnett <aburn1011@gmail.com>
2026-08-20 22:05:08 -0400

    Notepad++ Like Search Functionality

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,6 +11,36 @@ return
             vim.keymap.set('n', '<leader>fg', telescopeBuiltIn.live_grep, {})
             vim.keymap.set('n', '<leader>fb', telescopeBuiltIn.buffers, {})
             vim.keymap.set('n', '<leader>fh', telescopeBuiltIn.help_tags, {})
+
+            local live_grep_in_dir = function()
+                vim.ui.input({ prompt = 'Search Directory: ', default = '~/', completion = 'dir' }, function(input)
+                    if input == nil or input == '' then
+                        return
+                    end
+
+                    telescopeBuiltIn.live_grep({
+                        search_dirs = { input },
+                        prompt_title = 'Live Grep in ' .. input,
+                    })
+                end)
+            end
+
+            -- Notepad++ Like Search Functionality
+            local find_files_in_dir = function()
+                vim.ui.input({ prompt = 'Search Directory: ', default = '~/', completion = 'dir' }, function(input)
+                    if input == nil or input == '' then
+                        return
+                    end
+
+                    telescopeBuiltIn.find_files({
+                        search_dirs = { input },
+                        prompt_title = 'Find Files In ' .. input,
+                    })
+                end)
+            end
+
+            vim.keymap.set('n', '<leader>ft', live_grep_in_dir, { desc = 'Search for text in specific directory' })
+            vim.keymap.set('n', '<leader>sd', find_files_in_dir, { desc = 'Search for files in specific directory' })
         end
     }
 }
